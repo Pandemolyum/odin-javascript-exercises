d2473bee58267b2558150107fdcdc95c0ff536ef
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2025-10-02 13:01:52 -0400

    Complete Javascript exercises 6 to 11
    These exercises are focused on string and array manipulation.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...blacklist) {
+    for (item of blacklist) {
+        arr = arr.filter((num) => num !== item);
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((a < -1 || b < -1) ||
+        (a % 1 !== 0 || b % 1 !== 0) ||
+        (typeof a !== "number" || typeof b !== "number")) {
+            return "ERROR";
+        }
+    
+    let sum = 0;
 
+    for (i = Math.min(a,b); i <= Math.max(a,b); i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
